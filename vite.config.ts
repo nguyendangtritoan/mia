8758b1857f06429b93e3864f872d3a86ad12c9e1
Toan Nguyen <toan.nguyen@360t.com>
2025-11-22 00:14:54 +0100

    na: Fix path

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // IMPORTANT: This must match your GitHub repository name exactly
-  // If your repo is https://github.com/User/portfolio, this should be '/portfolio/'
-  // Example: if your repo is called "design-work", change this to: base: '/design-work/'
   base: '/my-portfolio/', 
 })
